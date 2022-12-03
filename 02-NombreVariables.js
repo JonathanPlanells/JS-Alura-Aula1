eb2879177a89b35b5b793e18cd7628222e1d15dd
JonathanPlanells <jonathanplanells@hotmail.com>
2022-12-03 00:16:21 -0500

    Curso iniciación JS OracleEducation

---
+++ 02-NombreVariables.js
@@ -0,0 +1,10 @@
+
+// NO COMENZAR CON MAYUSCULAS, NUMEROS O ACENTOS
+
+var nombre = "Juan";
+var nombreEmpleado = "Jose Perez";
+
+let telefono = 305890090
+let direccion = "Av.106 - Calle 5"
+const id = 1010109090
+
