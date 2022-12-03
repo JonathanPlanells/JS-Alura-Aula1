767b64f31625bbc8e5ab043e36c3edf767c7d5e7
JonathanPlanells <jonathanplanells@hotmail.com>
2022-12-03 00:27:56 -0500

    creación archivo buenas practicas

---
+++ 03-BuenasPracticas.js
@@ -0,0 +1,18 @@
+let nombreEmpleado = "Jose Perez";
+// diferente variable por las mayusculas
+let NombreEmpleado = "Pedro Ramírez"
+
+console.log(nombreEmpleado);
+console.log(NombreEmpleado);
+
+// creando una variable enterea o numero, JS la identifica automaticamente
+let edadEmpleado = 40;
+console.log(edadEmpleado)
+
+// Cambio de dato de la variable anteriormente creada
+edadEmpleado = "Cuarenta";
+console.log(edadEmpleado)
+
+// Mostrar errores
+// node en la terminal muestra los errores sin necesidad de buscarlos en la consola de Google Chrome
+// console.log(edadEmpleados) //-> ver error en la terminal
