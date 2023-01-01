a08cf2da3c1abb40b76511f7fcd3bfd8089420a4
JonathanPlanells <jonathanplanells@hotmail.com>
2022-12-31 23:06:07 -0500

    Se trabaja las condiciones con operadores logicos encadenados -- 2023 Lleno de cosas maravillosas -- Feliz Año

--- 11-OperadoresLogicosEncadenados.js
+++ 11-OperadoresLogicosEncadenados.js
@@ -2,14 +2,17 @@
 const ciudadDesitno = "Bogotá";
 const ciudadesDisponibles = new Array("Bogotá", "Lima", "Santiago", "Montevideo");
 
-let edadPasajero = 17;
+let edadPasajero = 18;
 let estaAcompañado = true;
+let tienePasaporte = true;
+let pareja = true;
 
 
-// A && B || C  -> A y B o C
+
+// A && B && C && D  -> A y B y C y D
 console.log(`Verificando pasajes para ${ciudadDesitno}`);
-if((ciudadesDisponibles.indexOf(ciudadDesitno)> - 1) && (edadPasajero >= 18 || estaAcompañado)){
-    console.log("Pasaje disponible para venta");
-} else{
+if(ciudadesDisponibles.indexOf(ciudadDesitno) > -1 && edadPasajero >= 18 && tienePasaporte && !pareja){
+    console.log("Pasaje disponible para la venta");
+} else{ 
     console.log("Ciudad no disponible para viajar o pasajero no cumple con las reglas")
 }
